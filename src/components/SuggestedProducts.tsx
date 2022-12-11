e6c80a9a3f466bf1eb634459857dc0c9c42034db
Michael Shepherd <53623238+mwshepherd@users.noreply.github.com>
2022-12-11 17:46:25 +1100

    fix custom slider navigation controls

--- src/components/SuggestedProducts.tsx
+++ src/components/SuggestedProducts.tsx
@@ -1,15 +1,13 @@
 import ProductCard from './ProductCard';
 import Container from './Container';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
+import { Navigation } from 'swiper';
 import { useRef } from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 const SuggestedProducts = ({ products }) => {
-  const navigationPrevRef = useRef(null);
-  const navigationNextRef = useRef(null);
   return (
     <Container>
       <div className="pt-20 flex flex-col items-center justify-center gap-8">
@@ -18,8 +16,8 @@ const SuggestedProducts = ({ products }) => {
           <Swiper
             modules={[Navigation]}
             navigation={{
-              prevEl: navigationPrevRef.current,
-              nextEl: navigationNextRef.current,
+              prevEl: '.prev-btn',
+              nextEl: '.next-btn',
             }}
             slidesPerView="auto"
             loop
@@ -43,18 +41,12 @@ const SuggestedProducts = ({ products }) => {
                 <ProductCard product={product} />
               </SwiperSlide>
             ))}
-            <div
-              ref={navigationPrevRef}
-              className="flex justify-center items-center bg-gray-800 w-10 h-10 absolute top-[50%] left-2 translate-y-[-100%] z-10 cursor-pointer opacity-50"
-            >
+            <div className="prev-btn flex justify-center items-center bg-gray-800 w-10 h-10 absolute top-[50%] left-2 translate-y-[-100%] z-10 cursor-pointer opacity-50">
               <svg width={10} height={18} viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.2 17l-8-8 8-8" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             </div>
-            <div
-              ref={navigationNextRef}
-              className="flex justify-center items-center bg-gray-800 w-10 h-10 absolute top-[50%] right-2 translate-y-[-100%] z-10 cursor-pointer opacity-50"
-            >
+            <div className="next-btn flex justify-center items-center bg-gray-800 w-10 h-10 absolute top-[50%] right-2 translate-y-[-100%] z-10 cursor-pointer opacity-50">
               <svg width={10} height={18} viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M.6 17l8-8-8-8" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
